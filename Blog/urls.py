bf4aaa179199fa4df3d0915a64edf1be8c2f274e
jacktembo <jack@jacktembo>
2022-05-07 23:46:57 +0200

    setting debug to false

--- Blog/urls.py
+++ Blog/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.AdminSite.site_title = 'Jack Tembo - Blog'
 admin.AdminSite.site_header = f"Jack Tembo's Blog - Administration"
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
